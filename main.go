efcc4aaee4d0eabd9b2b35e2c69baf07bdd901cd
famesensor <sathit.ju@outlook.com>
2023-05-15 21:44:05 +0700

    feat: init middleware, add health-check routes, add dependencies

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"log"
+
+	"github.com/famesensor/playground-go-line-sheet/routes"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
 func main() {
-	println("this is first log")
+	app := fiber.New()
+
+	app.Use(cors.New())
+	app.Use(compress.New())
+	app.Use(logger.New())
+
+	routes.HealthRoutes(app)
+
+	if err := app.Listen(":3000"); err != nil {
+		log.Fatal(err)
+	}
 }
