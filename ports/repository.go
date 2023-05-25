14f37cebcd576a8132448c7d664b73334d3966f7
famesensor <sathit.ju@outlook.com>
2023-05-25 22:06:53 +0700

    feat: update interface line,sheet repository

--- ports/repository.go
+++ ports/repository.go
@@ -1,5 +1,12 @@
 package ports
 
+import "github.com/famesensor/playground-go-line-sheet/models"
+
 type LineRepository interface {
+	SendMessageWords(replyToken string, words []models.Word) error
 	SendMessageError(replayToken string, msgErr string) error
+}
+
+type SheetRepository interface {
+	GetValues(spreadsheetId string, rangeSheet string) ([]models.Word, error)
 }
