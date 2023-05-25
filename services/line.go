0a8c6ce4991bbc5c07f26c6e330ac2fd8c7f270e
famesensor <sathit.ju@outlook.com>
2023-05-25 22:08:45 +0700

    feat: implement line service

--- services/line.go
+++ services/line.go
@@ -1,8 +1,36 @@
 package services
 
+import "github.com/famesensor/playground-go-line-sheet/ports"
+
 type LineService struct {
+	lineRepo      ports.LineRepository
+	sheetRepo     ports.SheetRepository
+	spreadSheetId string
 }
 
-func NewLineService() *LineService {
-	return &LineService{}
+func NewLineService(lineRepo ports.LineRepository, sheetRepo ports.SheetRepository, spreadSheetId string) *LineService {
+	return &LineService{
+		lineRepo,
+		sheetRepo,
+		spreadSheetId,
+	}
+}
+
+func (lineSrv *LineService) GetWords(replyToken string) {
+	words, err := lineSrv.sheetRepo.GetValues(lineSrv.spreadSheetId, "Wording!A:C")
+	if err != nil {
+		lineSrv.SendMessageError(replyToken)
+		return
+	}
+
+	err = lineSrv.lineRepo.SendMessageWords(replyToken, words)
+	if err != nil {
+		lineSrv.SendMessageError(replyToken)
+		return
+	}
+}
+
+func (lineSrv *LineService) SendMessageError(replyToken string) error {
+	message := "Operation is wrong or Internal server error"
+	return lineSrv.lineRepo.SendMessageError(replyToken, message)
 }
