8a0e92a160287077802cdbebeefc0c7d171ad7bf
famesensor <sathit.ju@outlook.com>
2023-05-25 22:09:01 +0700

    feat: implement main function

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"context"
 	"log"
 
+	"github.com/famesensor/playground-go-line-sheet/configs"
+	"github.com/famesensor/playground-go-line-sheet/controller"
+	"github.com/famesensor/playground-go-line-sheet/pkg/botline"
+	"github.com/famesensor/playground-go-line-sheet/pkg/sheet"
+	"github.com/famesensor/playground-go-line-sheet/repositories"
 	"github.com/famesensor/playground-go-line-sheet/routes"
+	"github.com/famesensor/playground-go-line-sheet/services"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -11,15 +18,38 @@ import (
 )
 
 func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	configs.InitViper()
+	config := configs.GetConfig()
+
 	app := fiber.New()
 
 	app.Use(cors.New())
 	app.Use(compress.New())
 	app.Use(logger.New())
 
-	routes.HealthRoutes(app)
+	// TODO: handle error
+	lintBot, _ := botline.ConnectLineBot(config)
+	sheetConn, _ := sheet.ConnectGoogleSheet(ctx, config)
 
-	if err := app.Listen(":3000"); err != nil {
+	// repositories
+	lineRepo := repositories.NewLineRepository(lintBot)
+	sheetRepo := repositories.NewSheetRepository(sheetConn)
+
+	// service
+	lineSrv := services.NewLineService(lineRepo, sheetRepo, config.SheetSpreadSheetId)
+
+	// controller
+	lineCtl := controller.NewLineController(lineSrv)
+
+	// routes
+	routes.HealthRoutes(app)
+	routes.LineRoutes(app, *lineCtl)
+
+	if err := app.Listen(":" + config.AppPort); err != nil {
 		log.Fatal(err)
 	}
 }
