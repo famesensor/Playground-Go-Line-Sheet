8b4b2e15bc6ccc8fcd9e5d39818b541dd544ac5c
famesensor <sathit.ju@outlook.com>
2023-06-05 21:29:45 +0700

    feat: add send operations, send add word desc

--- ports/repository.go
+++ ports/repository.go
@@ -3,7 +3,9 @@ package ports
 import "github.com/famesensor/playground-go-line-sheet/models"
 
 type LineRepository interface {
+	SendOperations(replyToken string) error
 	SendMessageWords(replyToken string, words []models.Word) error
+	SendAddWordDescription(replyToken string) error
 	SendMessageError(replayToken string, msgErr string) error
 }
 
--- repositories/line.go
+++ repositories/line.go
@@ -17,6 +17,51 @@ func NewLineRepository(lintBot *linebot.Client) *LineRepository {
 	}
 }
 
+func (r *LineRepository) SendOperations(replyToken string) error {
+	bubble := linebot.BubbleContainer{
+		Size: linebot.FlexBubbleSizeTypeKilo,
+		Header: &linebot.BoxComponent{
+			BackgroundColor: "#4CC764",
+			PaddingAll:      linebot.FlexComponentPaddingTypeXl,
+			Layout:          linebot.FlexBoxLayoutTypeVertical,
+			Contents: []linebot.FlexComponent{
+				&linebot.TextComponent{
+					Text:   "🌥️ Good morning wording",
+					Weight: linebot.FlexTextWeightTypeBold,
+					Color:  "#FFFFFF",
+				},
+			},
+		},
+		Body: &linebot.BoxComponent{
+			Layout:     linebot.FlexBoxLayoutTypeVertical,
+			PaddingAll: linebot.FlexComponentPaddingTypeLg,
+			Spacing:    linebot.FlexComponentSpacingTypeMd,
+			Contents: []linebot.FlexComponent{
+				&linebot.ButtonComponent{
+					Style:  linebot.FlexButtonStyleTypePrimary,
+					Action: linebot.NewMessageAction("Get words", "getwords"),
+					Height: linebot.FlexButtonHeightTypeSm,
+				},
+				&linebot.ButtonComponent{
+					Style:  linebot.FlexButtonStyleTypePrimary,
+					Action: linebot.NewMessageAction("Add word", "addword"),
+					Height: linebot.FlexButtonHeightTypeSm,
+				},
+			},
+		},
+	}
+
+	container := linebot.CarouselContainer{Contents: []*linebot.BubbleContainer{&bubble}}
+	flexMessage := linebot.NewFlexMessage("operations", &container)
+
+	_, err := r.lintBot.ReplyMessage(replyToken, flexMessage).Do()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (r *LineRepository) SendMessageWords(replyToken string, words []models.Word) error {
 	bubbles := []*linebot.BubbleContainer{}
 
@@ -25,9 +70,8 @@ func (r *LineRepository) SendMessageWords(replyToken string, words []models.Word
 			Size: linebot.FlexBubbleSizeTypeMicro,
 			Header: &linebot.BoxComponent{
 				BackgroundColor: "#4CC764",
-				PaddingAll:      linebot.FlexComponentPaddingTypeLg,
+				PaddingAll:      linebot.FlexComponentPaddingTypeXl,
 				Layout:          linebot.FlexBoxLayoutTypeVertical,
-				Spacing:         linebot.FlexComponentSpacingTypeSm,
 				Contents: []linebot.FlexComponent{
 					&linebot.TextComponent{
 						Text:   "🌥️ Good morning wording",
@@ -37,10 +81,9 @@ func (r *LineRepository) SendMessageWords(replyToken string, words []models.Word
 				},
 			},
 			Body: &linebot.BoxComponent{
-				Layout:          linebot.FlexBoxLayoutTypeVertical,
-				PaddingAll:      linebot.FlexComponentPaddingTypeLg,
-				Spacing:         linebot.FlexComponentSpacingTypeSm,
-				BackgroundColor: "#EFEFEF",
+				Layout:     linebot.FlexBoxLayoutTypeVertical,
+				PaddingAll: linebot.FlexComponentPaddingTypeLg,
+				Spacing:    linebot.FlexComponentSpacingTypeMd,
 				Contents: []linebot.FlexComponent{
 					&linebot.TextComponent{
 						Type:   linebot.FlexComponentTypeText,
@@ -68,6 +111,65 @@ func (r *LineRepository) SendMessageWords(replyToken string, words []models.Word
 
 	container := linebot.CarouselContainer{Contents: bubbles}
 	flexMessage := linebot.NewFlexMessage("words", &container)
+
+	_, err := r.lintBot.ReplyMessage(replyToken, flexMessage).Do()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *LineRepository) SendAddWordDescription(replyToken string) error {
+	bubble := linebot.BubbleContainer{
+		Size: linebot.FlexBubbleSizeTypeMega,
+		Header: &linebot.BoxComponent{
+			BackgroundColor: "#4CC764",
+			PaddingAll:      linebot.FlexComponentPaddingTypeXl,
+			Layout:          linebot.FlexBoxLayoutTypeVertical,
+			Contents: []linebot.FlexComponent{
+				&linebot.TextComponent{
+					Text:   "🌥️ Good morning wording",
+					Weight: linebot.FlexTextWeightTypeBold,
+					Color:  "#FFFFFF",
+				},
+			},
+		},
+		Body: &linebot.BoxComponent{
+			Layout:     linebot.FlexBoxLayoutTypeVertical,
+			PaddingAll: linebot.FlexComponentPaddingTypeXl,
+			Spacing:    linebot.FlexComponentSpacingTypeMd,
+			Contents: []linebot.FlexComponent{
+				&linebot.TextComponent{
+					Text:   "Add Word",
+					Size:   linebot.FlexTextSizeTypeMd,
+					Weight: linebot.FlexTextWeightTypeBold,
+				},
+				&linebot.BoxComponent{
+					Layout:  linebot.FlexBoxLayoutTypeBaseline,
+					Spacing: linebot.FlexComponentSpacingTypeMd,
+					Contents: []linebot.FlexComponent{
+						&linebot.TextComponent{
+							Text:  "Command",
+							Size:  linebot.FlexTextSizeTypeSm,
+							Color: "#aaaaaa",
+							Flex:  linebot.IntPtr(2),
+						},
+						&linebot.TextComponent{
+							Text:  "aw|\n\"word to add\"|\n\"meaning of word\"",
+							Size:  linebot.FlexTextSizeTypeSm,
+							Wrap:  true,
+							Color: "#666666",
+							Flex:  linebot.IntPtr(3),
+						},
+					},
+				},
+			},
+		},
+	}
+
+	container := linebot.CarouselContainer{Contents: []*linebot.BubbleContainer{&bubble}}
+	flexMessage := linebot.NewFlexMessage("operations", &container)
 
 	_, err := r.lintBot.ReplyMessage(replyToken, flexMessage).Do()
 	if err != nil {
